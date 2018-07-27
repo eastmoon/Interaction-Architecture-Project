4e0088d25b0fdd9460877e523106e7b1b184f2c1
jacky.chen <jacky.chen@yxunistar.com>
2018-07-27 15:52:11 +0800

    Add progress test cast.

---
+++ test/framework/patterns/progress.controller.js
@@ -0,0 +1,11 @@
+// Library, march and assert
+//import Assert from "assert";
+//import {assertFunction} from "utils/assert";
+
+// Test case
+describe('Framework.Progress, Pipe', () => {
+    it('Class method & Interface', () => {
+    });
+    it('Inherent & Constructor.', () => {
+    });
+});
