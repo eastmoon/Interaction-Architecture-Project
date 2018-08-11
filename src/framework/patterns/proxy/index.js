0c87b50bf22c827641994c6b2f13e2371f9c2a6a
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2018-08-12 01:49:48 +0800

    Add proxy pattern

--- src/framework/patterns/proxy/index.js
+++ src/framework/patterns/proxy/index.js
@@ -7,8 +7,94 @@ When Proxy state is change, it will notify to any traget who want to know.
 
 Proxy will have an ability like :
   1. Observer
-  2. Immutable (not necessary)
+  2. Immutable (not necessary, it need to use because javascript object deep-copy using pass by reference)
   3. Dynamic get/set attribute, and reflection method.
 
 author: jacky.chen
 */
+import Container from "framework/patterns/base/container";
+import Adapter from "framework/patterns/adapter";
+import Observer from "framework/patterns/observer";
+
+export default class Proxy extends Adapter.Immutable {
+    constructor($name) {
+        // 1. Call parent
+        super($name);
+        // 2. Initial proxy
+        //console.log("Generate Proxy", this.constructor.proxyDefined);
+        if (this.constructor.proxyDefined !== null && typeof this.constructor.proxyDefined !== "undefined") {
+            this.set(this.constructor.proxyDefined);
+        }
+        // 3. Initial observable object, it was a container which will register Observer.Subject.
+        this._observable = new Container(Observer.Subject);
+    }
+
+    // Observe accessor
+    // @param subject, a subject name, which is one accessor in this proxy.
+    // @param handler, when observe subject notify, this handler function will execute.
+    // @param name, a name to identify this handler.
+    observe($subject, $handler, $name) {
+        // 0. Check input parameter
+        if ($subject !== null && typeof $subject !== "string") {
+            return ;
+        }
+        if ($handler !== null && typeof $handler !== "function") {
+            return ;
+        }
+        if ($name !== null && typeof $name !== "string") {
+            return ;
+        }
+        // 1. if subject didn't exsit than generate it, else retrieve it.
+        let subject = null;
+        if (this._observable.has($subject)) {
+            subject = this._observable.retrieve($subject);
+        } else {
+            subject = new Observer.Subject($subject);
+            this._observable.register(subject);
+        }
+        // 2. register handler into subject.
+        subject.register($handler, $name);
+    }
+
+    // Remove Observe accessor
+    // @param subject, a subject name, which is one accessor in this proxy.
+    // @param handler, when observe subject notify, this handler function will execute.
+    // @param name, a name to identify this handler.
+    // NOTE: When handler and name is null, it mean remove a subject in observable.
+    removeObserve($subject, $handler = null, $name = null) {
+          // 0. Check input parameter
+          if ($subject !== null && typeof $subject !== "string") {
+              return;
+          }
+          // 1. Retrieve subject and remove one notification or all.
+          let subject = this._observable.retrieve($subject);
+          if (subject) {
+              // 1. if handler and name is null, remove subject.
+              if ($handler !== null && typeof $handler === "function" && $name !== null && typeof $name === "string") {
+                  subject.remove($handler, $name);
+              }
+              if ($handler === null && $name === null) {
+                  subject.removeAll();
+              }
+          }
+    }
+
+    // Overriding set property method
+    _setProperty($args) {
+        // 1. Save data with parent set accessor.
+        super._setProperty($args);
+        // 2. Notify this token, if token have some handler observer.
+        let token = null;
+        if ($args.node) {
+            token = `${$args.node}.${$args.key}`;
+        } else {
+            token = $args.key;
+        }
+        //console.log("Proxy set property", token);
+        // 3. Retrieve subject by token.
+        let subject = this._observable.retrieve(token);
+        if (subject) {
+            subject.notify(this);
+        }
+    }
+}
--- test/framework/patterns/proxy.js
+++ test/framework/patterns/proxy.js
@@ -1,54 +1,155 @@
 // Library, march and assert
-//import Assert from "assert";
-//import {assertFunction} from "utils/assert";
+import Assert from "assert";
+import {assertClass, assertFunction} from "utils/assert";
 
-class A {
-      constructor() {
-          // 1. Retrieve proxy defined in class.
-          this._dataSet = this.constructor.proxyDefined;
-          // 2. Register dynamice accessor.
-          Object.keys(this._dataSet).forEach((key) => {
-              Object.defineProperty(this, key, {
-                  get() {return this._dataSet[key]},
-                  set(value) {this._dataSet[key] = value},
-                  configurable: true,
-                  enumerable: true
-              });
-          });
-      }[]
-}
+// Framework Library
+import Proxy from "framework/patterns/proxy";
 
-class B extends A {
-    constructor() {
-        super();
-    }
-}
-
+// Test class & function
+class A extends Proxy {}
 A.proxyDefined = {
-    var1: 123,
-    var2: 456
+    x: 852,
+    y: 951,
+    z: 753
 }
 
-B.proxyDefined = {
-    var3: 999
+function foo($notify, $proxy) {
+    //console.log("Function Foo");
+    Assert.equal($proxy.x, 123);
+    Assert.equal($proxy.y, 951);
+    Assert.equal($proxy.z, 753);
+    $proxy.count += 10;
 }
 
-const t = new A();
-const o = new A();
-console.log(t.var1);
-console.log(t.var2);
-t.var1 = 789
-console.log(o.var1);
-const z = new B();
-console.log(z.var1);
-console.log(z.var2);
-console.log(z.var3);
-console.log(o.var1);
+function tar($notify, $proxy) {
+    //console.log("Function Bar");
+    Assert.equal($proxy.x, 123);
+    Assert.equal($proxy.y, 951);
+    Assert.equal($proxy.z, 753);
+    $proxy.count += 1;
+}
+
+function bar($notify, $proxy) {
+    //console.log("Function Bar");
+    Assert.equal($proxy.t, 789);
+    $proxy.x = 123;
+    $proxy.count += 5;
+}
 
 // Test case
-describe('Framework.Patterns.Proxy, ProgressController', () => {
+describe('Framework.Patterns.Proxy', () => {
     it('Class method & Interface', () => {
+        assertClass(Proxy);
+        assertFunction(Proxy.prototype.observe);
+        assertFunction(Proxy.prototype.removeObserve);
     });
-    it('Inherent & Constructor.', () => {
+    describe('Inherent & Constructor.', () => {
+        it(`Proxy initial with proxyDefined`, () => {
+            class B extends Proxy {}
+            B.proxyDefined = {
+                x: 123,
+                y: 456,
+                z: 789
+            };
+            const proxy = new B();
+            Assert.equal(proxy.x, 123);
+            Assert.equal(proxy.y, 456);
+            Assert.equal(proxy.z, 789);
+        });
+        it(`Proxy initial with set() method`, () => {
+            const proxy = new A();
+            proxy.set({
+                x: 123,
+                y: 456,
+                z: 789
+            });
+            Assert.equal(proxy.x, 123);
+            Assert.equal(proxy.y, 456);
+            Assert.equal(proxy.z, 789);
+        });
+        it(`Proxy initial mixing proxyDefault and set() method`, () => {
+            class B extends Proxy {}
+            B.proxyDefined = {
+                i: "ABC",
+                j: "DEF"
+            }
+            const proxy = new B();
+            proxy.set({
+                x: 123,
+                y: 456,
+                z: 789
+            });
+            Assert.equal(proxy.i, "ABC");
+            Assert.equal(proxy.j, "DEF");
+            Assert.equal(proxy.x, 123);
+            Assert.equal(proxy.y, 456);
+            Assert.equal(proxy.z, 789);
+        });
+    });
+    describe('Observe proxy attribute', () => {
+        it(`Observe one attribute, and check result change.`, () => {
+            const proxy = new A();
+            proxy.set({count: 0});
+            Assert.equal(proxy.x, 852);
+            Assert.equal(proxy.y, 951);
+            Assert.equal(proxy.z, 753);
+            proxy.observe("x", foo, "window");
+            proxy.x = 123;
+            Assert.equal(proxy.count, 10);
+        });
+        it(`Observe multi-attribute, and use one to trigger another.`, () => {
+            const proxy = new A();
+            proxy.set({t: 0, count: 0});
+            Assert.equal(proxy.x, 852);
+            Assert.equal(proxy.y, 951);
+            Assert.equal(proxy.z, 753);
+            proxy.observe("x", foo, "window");
+            proxy.observe("t", bar, "window");
+            proxy.t = 789;
+            Assert.equal(proxy.count, 15);
+        });
+    });
+    describe('Remove observe', () => {
+        it(`Remove one by one.`, () => {
+            // initial
+            const proxy = new A();
+            proxy.set({count: 0});
+            // check observe
+            Assert.equal(proxy.x, 852);
+            Assert.equal(proxy.y, 951);
+            Assert.equal(proxy.z, 753);
+            proxy.observe("x", foo, "window");
+            proxy.observe("x", tar, "window");
+            proxy.x = 123;
+            Assert.equal(proxy.count, 11);
+            // remove one
+            proxy.count = 0;
+            proxy.removeObserve("x", foo, "window");
+            proxy.x = 123;
+            Assert.equal(proxy.count, 1);
+            // remove other one
+            proxy.set({count: 0});
+            proxy.removeObserve("x", tar, "window");
+            proxy.x = 123;
+            Assert.equal(proxy.count, 0);
+        });
+        it(`Remove subject.`, () => {
+            // initial
+            const proxy = new A();
+            proxy.set({count: 0});
+            // check observe
+            Assert.equal(proxy.x, 852);
+            Assert.equal(proxy.y, 951);
+            Assert.equal(proxy.z, 753);
+            proxy.observe("x", foo, "window");
+            proxy.observe("x", tar, "window");
+            proxy.x = 123;
+            Assert.equal(proxy.count, 11);
+            // remove subject
+            proxy.count = 0;
+            proxy.removeObserve("x");
+            proxy.x = 123;
+            Assert.equal(proxy.count, 0);
+        });
     });
 });
